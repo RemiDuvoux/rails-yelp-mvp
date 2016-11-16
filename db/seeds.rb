62797e6a69bda245745c25c1a97e2c91ba5ff6a7
Remi Duvoux <remi.duvoux@gmail.com>
2016-11-16 13:16:19 +0100

    added some faker restaurants to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  Restaurant.create!(address: Faker::GameOfThrones.city, name: Faker::Company.name, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  2.times do
+    Review.create!(content: Faker::Hipster.sentence, rating: (0..5).to_a.sample, restaurant_id: Restaurant.last.id)
+  end
+end
